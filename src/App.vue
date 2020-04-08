a63f54e7c744981c79c8838a3bdc1d9cfe729acd
张峻郡 <zhangjunjun@xinhua.org>
2020-04-08 22:31:00 +0800

    my mobile template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,74 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <transition :name="transitionName">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </transition>
   </div>
 </template>
 
+<script>
+export default {
+  name: "app",
+  watch: {
+    $route() {
+      let isBack = this.$router.isBack; //监听是后退还是跳转
+      if (isBack) {
+        this.transitionName = "slide-left";
+      } else {
+        this.transitionName = "slide-right";
+      }
+      this.$router.isBack = false;
+    }
+  },
+  data() {
+    return {
+      transitionName: "slide-right"
+    };
+  },
+  mounted() {
+    window.addEventListener("popstate", () => {
+      this.$router.isBack = true;
+    });
+  },
+  components: {}
+};
+</script>
+
 <style lang="scss">
+html,
+body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  width: 100%;
+  height: 100%;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  .slide-left-enter,
+  .slide-right-leave-to {
+    opacity: 0;
+    transform: translateX(100%);
+  }
 
-#nav {
-  padding: 30px;
+  .slide-left-leave-to,
+  .slide-right-enter {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .slide-left-enter-active,
+  .slide-left-leave-active,
+  .slide-right-enter-active,
+  .slide-right-leave-active {
+    transition: 1s;
+    position: absolute;
+    top: 0;
   }
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
+  <div class="about" @click="toHome">
+    <van-button type="primary">back</van-button>
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  methods: {
+    toHome() {
+      this.$router.back();
+    }
+  }
+};
+</script>
+<style lang='scss' scoped>
+</style>
